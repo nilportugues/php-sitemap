fea3eb33b0533b6100123a4005eae4633b7dc2c3
Nil Portugués Calderó <contact@nilportugues.com>
2014-02-05 16:59:55 +0000

    More testing

--- tests/Sonrisa/Component/Sitemap/XMLSitemapTest.php
+++ tests/Sonrisa/Component/Sitemap/XMLSitemapTest.php
@@ -1139,4 +1139,133 @@ XML;
      *-------------------------------------------------------------------------*/
 
 
+
+
+    public function testAddUrlAndImagesWithValidUrlForImages()
+    {
+        $expected=<<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+\t<url>
+\t\t<loc>http://www.example.com/</loc>
+\t\t<lastmod>2005-05-10T17:33:30+08:00</lastmod>
+\t\t<changefreq>monthly</changefreq>
+\t\t<priority>0.8</priority>
+\t</url>
+</urlset>
+XML;
+        $this->sitemap->addUrl('http://www.example.com/','0.8','monthly','2005-05-10T17:33:30+08:00');
+        $this->sitemap->addImage('http://www.example.com/',array('loc' => 'no/a/proper/url', 'title' => 'Example.com logo' ));
+        $files = $this->sitemap->build()->get();
+        $this->assertEquals($expected,$files[0]);
+    }
+
+    public function testAddUrlAndImagesWithNoUrlForImages()
+    {
+        $expected=<<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+\t<url>
+\t\t<loc>http://www.example.com/</loc>
+\t\t<lastmod>2005-05-10T17:33:30+08:00</lastmod>
+\t\t<changefreq>monthly</changefreq>
+\t\t<priority>0.8</priority>
+\t</url>
+</urlset>
+XML;
+        $this->sitemap->addUrl('http://www.example.com/','0.8','monthly','2005-05-10T17:33:30+08:00');
+        $this->sitemap->addImage('http://www.example.com/',array('title' => 'Example.com logo' ));
+        $files = $this->sitemap->build()->get();
+        $this->assertEquals($expected,$files[0]);
+    }
+
+    public function testAddUrlAndImagesWithValidUrlForImagesAndOtherImageDataPassedIsEmpty()
+    {
+        $expected=<<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+\t<url>
+\t\t<loc>http://www.example.com/</loc>
+\t\t<lastmod>2005-05-10T17:33:30+08:00</lastmod>
+\t\t<changefreq>monthly</changefreq>
+\t\t<priority>0.8</priority>
+\t\t<image:image>
+\t\t\t<image:loc><![CDATA[http://www.example.com/logo.png]]></image:loc>
+\t\t</image:image>
+\t</url>
+</urlset>
+XML;
+        $this->sitemap->addUrl('http://www.example.com/','0.8','monthly','2005-05-10T17:33:30+08:00');
+        $this->sitemap->addImage('http://www.example.com/',array('loc' => 'http://www.example.com/logo.png', 'title' => '', 'geolocation' => '', 'license' => '', 'caption' =>'' ));
+        $files = $this->sitemap->build()->get();
+        $this->assertEquals($expected,$files[0]);
+    }
+
+    public function testAddUrlAndImagesWithValidUrlAndGeolocationForImages()
+    {
+        $expected=<<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+\t<url>
+\t\t<loc>http://www.example.com/</loc>
+\t\t<lastmod>2005-05-10T17:33:30+08:00</lastmod>
+\t\t<changefreq>monthly</changefreq>
+\t\t<priority>0.8</priority>
+\t\t<image:image>
+\t\t\t<image:loc><![CDATA[http://www.example.com/logo.png]]></image:loc>
+\t\t\t<image:geolocation><![CDATA[Limerick, Ireland]]></image:geolocation>
+\t\t</image:image>
+\t</url>
+</urlset>
+XML;
+        $this->sitemap->addUrl('http://www.example.com/','0.8','monthly','2005-05-10T17:33:30+08:00');
+        $this->sitemap->addImage('http://www.example.com/',array('loc' => 'http://www.example.com/logo.png', 'geolocation' => 'Limerick, Ireland' ));
+        $files = $this->sitemap->build()->get();
+        $this->assertEquals($expected,$files[0]);
+    }   
+
+    public function testAddUrlAndImagesWithValidUrlAndLicenseForImages()
+    {
+        $expected=<<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+\t<url>
+\t\t<loc>http://www.example.com/</loc>
+\t\t<lastmod>2005-05-10T17:33:30+08:00</lastmod>
+\t\t<changefreq>monthly</changefreq>
+\t\t<priority>0.8</priority>
+\t\t<image:image>
+\t\t\t<image:loc><![CDATA[http://www.example.com/logo.png]]></image:loc>
+\t\t\t<image:license><![CDATA[MIT]]></image:license>
+\t\t</image:image>
+\t</url>
+</urlset>
+XML;
+        $this->sitemap->addUrl('http://www.example.com/','0.8','monthly','2005-05-10T17:33:30+08:00');
+        $this->sitemap->addImage('http://www.example.com/',array('loc' => 'http://www.example.com/logo.png', 'license' => 'MIT' ));
+        $files = $this->sitemap->build()->get();
+        $this->assertEquals($expected,$files[0]);
+    }   
+    public function testAddUrlAndImagesWithValidUrlAndCaptionForImages()
+    {
+        $expected=<<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+\t<url>
+\t\t<loc>http://www.example.com/</loc>
+\t\t<lastmod>2005-05-10T17:33:30+08:00</lastmod>
+\t\t<changefreq>monthly</changefreq>
+\t\t<priority>0.8</priority>
+\t\t<image:image>
+\t\t\t<image:loc><![CDATA[http://www.example.com/logo.png]]></image:loc>
+\t\t\t<image:caption><![CDATA[This place is called Limerick, Ireland]]></image:caption>
+\t\t</image:image>
+\t</url>
+</urlset>
+XML;
+        $this->sitemap->addUrl('http://www.example.com/','0.8','monthly','2005-05-10T17:33:30+08:00');
+        $this->sitemap->addImage('http://www.example.com/',array('loc' => 'http://www.example.com/logo.png', 'caption' => 'This place is called Limerick, Ireland' ));
+        $files = $this->sitemap->build()->get();
+        $this->assertEquals($expected,$files[0]);
+    }   
 }
