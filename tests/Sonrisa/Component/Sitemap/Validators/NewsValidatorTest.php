9db62079f3753fbe23e9138cd98358a70a377db8
Nil Portugués Calderó <nilopc@gmail.com>
2014-02-26 00:02:48 +0100

    Added strict validation for news

--- tests/Sonrisa/Component/Sitemap/Validators/NewsValidatorTest.php
+++ tests/Sonrisa/Component/Sitemap/Validators/NewsValidatorTest.php
@@ -39,4 +39,58 @@ class NewsValidatorTest  extends \PHPUnit_Framework_TestCase
         $this->assertEquals('',$result);
     }
 
+
+    public function testValidateLanguageISO639_1()
+    {
+        $result = $this->validator->validateLanguage('ca');
+        $this->assertEquals('ca',$result);
+    }
+
+    public function testValidateLanguageISO639_2()
+    {
+        $result = $this->validator->validateLanguage('cat');
+        $this->assertEquals('cat',$result);
+    }
+
+    public function testValidateLanguageISO639_3()
+    {
+        $result = $this->validator->validateLanguage('aaa');
+        $this->assertEquals('aaa',$result);
+    }
+
+    public function testValidateAccessSubscription()
+    {
+        $result = $this->validator->validateAccess('Subscription');
+        $this->assertEquals('Subscription',$result);
+    }
+
+    public function testValidateAccessRegistration()
+    {
+        $result = $this->validator->validateAccess('Registration');
+        $this->assertEquals('Registration',$result);
+    }
+
+    public function testValidateAccessInvalid()
+    {
+        $result = $this->validator->validateAccess('');
+        $this->assertEquals('',$result);
+    }
+
+    public function testValidateGenresMethod1()
+    {
+        $result = $this->validator->validateGenres('PressRelease, Satire, Blog, OpEd , Opinion, UserGenerated');
+        $this->assertEquals('PressRelease, Satire, Blog, OpEd, Opinion, UserGenerated',$result);
+    }
+
+    public function testValidateGenresMethod2()
+    {
+        $result = $this->validator->validateGenres(array('PressRelease','Satire','Blog','OpEd','Opinion','UserGenerated'));
+        $this->assertEquals('PressRelease, Satire, Blog, OpEd, Opinion, UserGenerated',$result);
+    }
+
+    public function testValidateGenresSkipInvalid()
+    {
+        $result = $this->validator->validateGenres(array('PreXXXXssRelease','Satire','Blog','OpEd','Opinion','UserGenerated'));
+        $this->assertEquals('Satire, Blog, OpEd, Opinion, UserGenerated',$result);
+    }
 } 
