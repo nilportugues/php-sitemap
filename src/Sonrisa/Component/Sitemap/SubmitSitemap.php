bbc34879162a078eb8a258e7d9e6b35e9b8c2107
Nil Portugués Calderó <nilopc@gmail.com>
2014-02-27 19:54:17 +0100

    Fix for [Insight] PHP code should follow PSR-1 basic coding standard #5

--- src/Sonrisa/Component/Sitemap/SubmitSitemap.php
+++ src/Sonrisa/Component/Sitemap/SubmitSitemap.php
@@ -34,7 +34,7 @@ class SubmitSitemap
         //Validate URL format and Response
         if ( filter_var( $url, FILTER_VALIDATE_URL, array('options' => array('flags' => FILTER_FLAG_PATH_REQUIRED)) ) ) {
             if (self::sendHttpHeadRequest($url) === true ) {
-                return self::do_submit($url);
+                return self::submitSitemap($url);
             }
             throw new SitemapException("The URL provided ({$url}) holds no accessible sitemap file.");
         }
@@ -47,7 +47,7 @@ class SubmitSitemap
      * @param $url string 		Valid URL being submitted.
      * @return array Array with the search engine submission success status as a boolean.
      */
-    protected static function do_submit($url)
+    protected static function submitSitemap($url)
     {
         $response = array();
 
