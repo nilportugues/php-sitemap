d7994b714fc0f2fc124ef7a47dc5821c2e0c2e87
SonrisaCMS <contact@nilportugues.com>
2014-02-28 00:46:03 +0100

    Update test.php

--- test.php
+++ test.php
@@ -2,16 +2,21 @@
 
 include 'vendor/autoload.php';
 
-$sitemap = new \Sonrisa\Component\Sitemap\VideoSitemap();
-
 $validator = new \Sonrisa\Component\Sitemap\Validators\VideoValidator();
 $item = new \Sonrisa\Component\Sitemap\Items\VideoItem($validator);
+$sitemap = new \Sonrisa\Component\Sitemap\VideoSitemap();
 
-$item->setTitle('hello title')
-     ->setContentLoc('http://google.com/video.mp4');
+try
+{
+    $item->setTitle('hello title')
+         ->setContentLoc('http://google.com/video.mp4');
 
-$sitemap->add($item,'http://google.com');
+    $sitemap->add($item,'http://google.com');
 
-$data = $sitemap->build();
-
-var_dump($data);
+    $data = $sitemap->build();
+    var_dump($data);    
+}
+catch (\Sonrisa\Component\Sitemap\Exceptions\SitemapException $e)
+{
+    echo $e->getMessage();    
+}
