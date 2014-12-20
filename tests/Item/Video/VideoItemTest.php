b2840d26b299b4ae463bfe6f48182cb7bf2af44b
Nil Portugués Calderó <nilopc@gmail.com>
2014-12-20 22:37:08 +0100

    Forgot to remove tests for VideoItem

--- tests/Item/Video/VideoItemTest.php
+++ tests/Item/Video/VideoItemTest.php
@@ -91,27 +91,6 @@ class VideoItemTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function itShouldOutputHeader()
-    {
-        $this->assertSame(
-            '<?xml version="1.0" encoding="UTF-8"?>' . "\n"
-            . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"'
-            . ' xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">' . "\n",
-            $this->item->getHeader()
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function itShouldOutputFooter()
-    {
-        $this->assertSame('</urlset>', $this->item->getFooter());
-    }
-
-    /**
-     * @test
-     */
     public function itShouldHaveThumbnailLoc()
     {
         $this->item->setThumbnailLoc('http://www.example.com/thumbs/123.jpg');
